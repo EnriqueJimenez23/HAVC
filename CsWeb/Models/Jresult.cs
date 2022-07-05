7d3c252571422c0777750c34adc554a0fab44bf5
Rafael Pedraza <rafaelpedraza1810@gmail.com>
2022-07-05 15:31:24 -0500

    fix

---
+++ CsWeb/Models/Jresult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CsWeb.Models
+{
+    /// <summary>
+    /// Definición de objeto que representa el resultado de una operacion Ajax en el controlador
+    /// </summary>
+    public class Jresult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public dynamic Result { get; set; }
+
+        public Jresult()
+        {
+            Success = false;
+            Message = string.Empty;
+        }
+    }
+}
